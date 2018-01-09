a5d7e3e845a6fc583f4f0bea82a5ab4d7ead9977
Fabian Irsara <info@fabianirsara.com>
2018-01-09 14:52:37 +0100

    chore(core): setup initial initial development environment

---
+++ .cz-config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  allowBreakingChanges: ['feat'],
+  allowCustomScopes: false,
+  scopes: [
+    'lib',
+    'styles',
+    'utils'
+  ],
+  types: [
+    {value: 'feat',     name: 'feat:     A new feature'},
+    {value: 'fix',      name: 'fix:      A bug fix'},
+    {value: 'docs',     name: 'docs:     Documentation only changes'},
+    {value: 'refactor', name: 'refactor: A code change that neither fixes a bug nor adds a feature'},
+    {value: 'test',     name: 'test:     Adding missing tests'},
+    {value: 'chore',    name: 'chore:    Changes to the build process or auxiliary tools and libraries such as documentation generation'},
+    {value: 'revert',   name: 'revert:   Revert to a commit'}
+  ]
+};
---
+++ README.md
@@ -0,0 +1,30 @@
+# Revealer
+
+## Installation
+
+### Install Yarn
+
+```
+brew update
+brew install yarn
+```
+
+### Install packages
+```
+yarn install
+```
+
+## Start Dev-Server
+
+```
+yarn start
+```
+
+## Contribute
+
+### Install commitizen
+
+```
+npm i -g commitizen
+npm i
+```
